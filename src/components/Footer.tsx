ef2eb1e069443d19b1ea50b817c8925ee35cd11a
Jetskiis <yegene60@gmail.com>
2022-11-17 16:14:22 -0500

    initialize new project with ts config

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <Footer>
+        <p>Copyright &Copyright 2022 Jetskiis</p>
+        <link href="" target="_blank"></link>
+    </Footer>
+  )
+}
