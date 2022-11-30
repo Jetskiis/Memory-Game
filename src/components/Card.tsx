ae6c4e4048c23050d0b8cb39e6db2a1287b50108
Jetskiis <yegene60@gmail.com>
2022-11-29 19:25:41 -0500

    add memory game logic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,15 @@
+import React from "react";
 import "../styles/Card.css"
 
 interface CardProps{
     img: string;
     name: string;
-    onClick: () => void;
+    onClick: (e: React.MouseEvent) => void;
 }
 
 const Card = (props: CardProps) => {
     return(
-    <div className="card" onClick={props.onClick}>
+    <div className="card" onClick={props.onClick} id={props.name}>
         <img src={props.img} alt=""/>
         <span>{props.name}</span>
     </div>
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Card from "./Card";
 
 interface CardContainerProps {
@@ -7,30 +6,65 @@ interface CardContainerProps {
   setCards: (cards: {}) => void;
 }
 
-// const numCards = 16;
-// const cardDict = {};
+const numCards: number = 16;
+const cardDict: any = {
+  abra: "img/pokemon_img/abra.png",
+  bulbasaur: "img/pokemon_img/bulbasaur.png",
+  caterpie: "img/pokemon_img/caterpie.png",
+  charmander: "img/pokemon_img/charmander.png",
+  clefairy: "img/pokemon_img/clefairy.png",
+  ekans: "img/pokemon_img/ekans.png",
+  jigglypuff: "img/pokemon_img/jigglypuff.png",
+  machop:"img/pokemon_img/machop.png",
+  nidoran: "img/pokemon_img/nidoran.png",
+  pidgey: "img/pokemon_img/pidgey.png",
+  pikachu: "img/pokemon_img/pikachu.png",
+  rattata: "img/pokemon_img/rattata.png",
+  sandshrew: "img/pokemon_img/sandshrew.png",
+  squirtle: "img/pokemon_img/squritle.png",
+  vulpix: "img/pokemon_img/vulpix.png",
+  weedle: "img/pokemon_img/weedle.png",
+};
 
 
 const CardContainer = (props: CardContainerProps) => {
   useEffect(() => {
-    if(Object.keys(props.cards).length === 12) {
+    if(Object.keys(props.cards).length === numCards) {
       alert("You win!");
       props.setCards({});
     }
   },[props]);  
 
+  function handleClick(e: React.MouseEvent) {
+    const cardName: string | null = (e.target as HTMLElement).id;  
+    // console.log(cardName);
+    if(cardName) {
+    if((props.cards as any)[cardName]) {
+      props.setCards({});
+    } else {
+      props.setCards({...props.cards, [cardName]: true});
+    }
+  }
+  }
+
 
   let output: JSX.Element[] = [];
-//  let alreadySelected: number[] = Array(numCards).fill(0);
-//   for(let i = 0; i < 9; i++) {
+ let alreadySelected: number[] = Array(numCards).fill(0);
 
-//     output.push(<Card key={i} img={} onClick={} name={}/>);
-//   }
+  for(let i = 0; i < 16; i++) {
+    let randomNum: number = Math.floor(Math.random() * numCards);
+    while(alreadySelected[randomNum] === 1) {
+      randomNum = Math.floor(Math.random() * numCards);
+    }
+    alreadySelected[randomNum] = 1;
+    let randomCardName: string = Object.keys(cardDict)[randomNum];
+    output.push(<Card key={i} img={cardDict[randomCardName]} onClick={handleClick} name={randomCardName}/>);
+  }
 
   return (
     <div className="card-container">
       {output}
-      <Card key={1} img={"img/pokemon_img/1891631-016pidgey.png"} onClick={()=>{}} name={"Pidgey"}></Card>
+      {/* <Card key={1} img={"img/pokemon_img/1891631-016pidgey.png"} onClick={()=>{}} name={"Pidgey"}></Card> */}
     </div>
   )
 }
